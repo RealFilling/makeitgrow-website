4417433789775253f5bd82886ed92367f489eeaf
Leandro Ostera <leostera@gmail.com>
2012-09-17 06:47:12 -0300

    Deleted unnecesary stuff.

--- config.php
+++ config.php
@@ -33,8 +33,6 @@ $config["fb_fields"] = array(
     	array("name" => "name")
     ,	array("name" => "email")
     ,   array("name" => "farmname", "description" => "How will you call your farm?", "type" => "text")
-    //,	array("name" => "gender")
-    //,	array("name" => "birthday")
 );
 
 $facebook = new Facebook($config);
--- register.php
+++ register.php
@@ -4,7 +4,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Make It Grow</title>
-<link rel="stylesheet" type="text/css" href="css/main.css" />
 </head>
 
 <body>
