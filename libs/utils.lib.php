573879c9e50ae218bea167c91f87b603290d43a2
Leandro Ostera <leostera@gmail.com>
2012-09-17 05:00:34 -0300

    Cleaned up utils.

--- libs/utils.lib.php
+++ libs/utils.lib.php
@@ -1,23 +1,6 @@
 <?php
 
-function formatData($status, $data)
-{
-	return "{ \"result\": { \"status\": \"$status\", \"data\": \"$data\" }	}";
-}
-
-function filterInput($input)
-{
-	return htmlspecialchars(trim($input));
-}
-
 function getSubdomain()
 {
 	return explode('.',$_SERVER['SERVER_NAME'])[0];
-}
-
-function sendMail($to, $subject, $message)
-{
-	$headers = "From: contact@DOMAIN\r\n"."X-Mailer: php";
-	$res = mail($to , $subject , $message, $headers);
-	return ($res || false);
 }
