219c076e0564204286dad367abee706d3b5f594b
Leandro Ostera <leostera@gmail.com>
2012-09-20 16:13:30 -0300

    Refactored utils from functions.php to utils.lib.php

--- libs/utils.lib.php
+++ libs/utils.lib.php
@@ -5,3 +5,14 @@ function getSubdomain()
 	$exploded_domain = explode('.',$_SERVER['SERVER_NAME']);
 	return $exploded_domain[0];
 }
+
+function retrieve_fields($sf)
+{
+    return json_encode($sf);
+}
+
+function verify_fields($f,$sf)
+{
+	$fields = json_encode($sf);
+    return (strcmp($fields,$f) === 0);
+}
