c7bd981ef88a2160ed5dfbfebb9cd9a4f06c6919
John William Harris <johnwh@gmail.com>
2012-10-02 17:33:04 -0400

    Debug save/load keys reenabled
    Q to save, W to load

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ if ($me != 0) {
       </div>
 
       <!-- Run the game code -->
-        <script type="text/javascript" src="html5game/index.js?XLDAC=972105764"></script>
+        <script type="text/javascript" src="html5game/index.js?YSFYB=332375152"></script>
     </section>
 
 
