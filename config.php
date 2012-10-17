4a22206170f35ba4de44e2783225bf2b2d383b2c
Leandro Ostera <leostera@gmail.com>
2012-10-17 02:30:13 -0300

    Latest version from dev game.gmx

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ define('FB_SECRET','a064f8011c4d66800993118437e5e4e6');
 define('FB_REDIRECT',"http://dev.makeitgrowgame.com");
 
 // Game Maker Export Key
-define('GAME_KEY', 'NXJYB=234245824');
+define('GAME_KEY', 'GONAC=119204980');
 
 // Connect to the database
 mysql_connect(DB_HOST, DB_USR, DB_PWD) or die("MySQL Error: " . mysql_error());
