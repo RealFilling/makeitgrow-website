04838bf5ef110e6c32b302466d8425c7777e77b7
Leandro Ostera <leostera@gmail.com>
2012-10-31 13:05:55 -0200

    Installed Devise and OmniAuth.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeitgrowWebsite::Application.config.secret_token = '8e2357c487c13e39085ee2b475e6714bc214b3072dec15201f8bf38f91efdb7ada5128be39f1bd9c1ca3182952984433a80d126eba3becfe1f18eff9c8d5c893'
