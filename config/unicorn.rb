4508269055a77d92bd9eace21d94866d308c97b7
Leandro Ostera <leostera@gmail.com>
2012-11-02 03:57:11 -0200

    Listening to address:port instead of unix socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "/home/app/makeitgrow/tmp/pids/unicorn.pid"
 stderr_path "/home/app/makeitgrow/log/unicorn.log"
 stdout_path "/home/app/makeitgrow/log/unicorn.log"
 
-listen "/tmp/unicorn.sock"
+listen "127.0.0.1:8000"
 worker_processes 2
 timeout 30
