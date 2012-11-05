c3a09c1b118355c17fbf53f037938d4be4a4f8e0
Leandro Ostera <leostera@gmail.com>
2012-11-05 20:17:21 -0300

    Capified and binstubbed.

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-set -e
+
+# set -e
+
 # Example init script, this can be used with nginx, too,
 # since nginx and unicorn accept the same signals
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/app/makeitgrow
+# APP_ROOT=/home/app/makeitgrow
+APP_ROOT=/vagrant
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn_rails -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
