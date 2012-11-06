2c699b64cfd71b729481708b91fb541b055a5410
Leandro Ostera <leostera@gmail.com>
2012-11-06 05:25:45 -0300

    deploy/assets has its own capistrano recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require "bundler/capistrano"
+load "deploy/assets"
 
 set :application, "makeitgrow"
 
@@ -34,19 +35,7 @@ namespace :deploy do
     run "sudo service unicorn reload"
   end
 
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-
-
-  desc "Post Setup"
+  desc "Prepare for Battle!"
   task :post_setup do
     # Install unicorn
     run "chmod a+x #{current_path}/config/unicorn_init.sh"
@@ -61,4 +50,3 @@ namespace :deploy do
 end
 
 after "deploy:create_symlink", "deploy:post_setup"
-after "deploy:post_setup", "deploy:assets:precompile"
