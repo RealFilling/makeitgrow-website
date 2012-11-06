328a723eff75739ea491b31b8707185541bbd071
Leandro Ostera <leostera@gmail.com>
2012-11-06 00:49:28 -0300

    Started capifying.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,10 @@
-set :application, "Make It Grow Website"
+set :application, "makeitgrow"
 set :repository,  "http://github.com/RealFiling/makeitgrow-website.git"
 set :scm, :git
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "makeitgrowgame.com"                          # Your HTTP server, Apache/etc
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
