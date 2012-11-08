826a2ee7db8c76401cbee0ebb8e35efd75273d3e
Leandro Ostera <leostera@gmail.com>
2012-11-07 22:58:04 -0300

    Added provider and userid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username,
+                    :provider, :uid
   
   validates_presence_of :username
   validates_uniqueness_of :username
