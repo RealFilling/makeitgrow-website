fa23721f6cafe6580d51761fa098a253674e2648
Leandro Ostera <leostera@gmail.com>
2012-11-08 18:40:49 -0300

    Finished installing Devise. Added Game Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MakeitgrowWebsite::Application.routes.draw do
+  
+  match "game" => "game#index"
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
                      controllers: {omniauth_callbacks: "omniauth_callbacks"}
   root :to => "home#index"
+
 end
