c8e79c66599b9e7c253a0e8b2dbb1d889822c6e4
Leandro Ostera <leostera@gmail.com>
2012-11-10 19:43:49 -0300

    Removed before_filter

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,4 @@
 class GameController < ApplicationController
-  before_filter :authenticate_user!, :except => :index
-  
   def index
     if user_signed_in?
       game = Savegame.where("uid == #{current_user.uid}").order('timestamp DESC').limit(1)
@@ -13,8 +11,6 @@ class GameController < ApplicationController
   def save
     if user_signed_in?
       Savegame.create!(:gamestate => params[:gamestate], :hypetime => params[:hypertime], :uid => current_user.uid)
-    else
-      respond_with
     end
   end
 end
